258496ee73aae0a72a2d661b66885520bf84e4d0
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-10-28 22:24:28 +0200

    Add hooks using

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Box } from './Box';
 import Section from './Section';
 import FeedbackOptions from './FeedbackOptions';
@@ -7,50 +7,41 @@ import Notification from './Notification';
 import { countTotalFeedback, countPositiveFeedbackPercentage } from '../utils';
 import noResultsImg from '../img/no-result.png';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [grades, setGrades] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const saveFeedback = grade => {
+    setGrades(prevState => ({ ...prevState, [grade]: prevState[grade] + 1 }));
   };
 
-  saveFeedback = grade => {
-    this.setState(prevState => ({ [grade]: prevState[grade] + 1 }));
-  };
+  const options = Object.keys(grades);
+  const total = countTotalFeedback(grades);
+  const positivePercentage = countPositiveFeedbackPercentage(grades);
 
-  render() {
-    const options = Object.keys(this.state);
-    const total = countTotalFeedback(this.state);
-    const positivePercentage = countPositiveFeedbackPercentage(this.state);
+  return (
+    <Box
+      p="30px"
+      bg="white"
+      width="390px"
+      m="10px auto"
+      display="grid"
+      gridGap="30px"
+    >
+      <Section title="Please leave feedback" firstLevelTitle={true}>
+        <FeedbackOptions options={options} onLeaveFeedback={saveFeedback} />
+      </Section>
 
-    return (
-      <Box
-        p="30px"
-        bg="white"
-        width="390px"
-        m="10px auto"
-        display="grid"
-        gridGap="30px"
-      >
-        <Section title="Please leave feedback" firstLevelTitle={true}>
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.saveFeedback}
+      {total ? (
+        <Section title="Statistics">
+          <Statistics
+            statistics={grades}
+            total={total}
+            positivePercentage={positivePercentage}
           />
         </Section>
-
-        {total ? (
-          <Section title="Statistics">
-            <Statistics
-              statistics={this.state}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" image={noResultsImg} />
-        )}
-      </Box>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" image={noResultsImg} />
+      )}
+    </Box>
+  );
+};
